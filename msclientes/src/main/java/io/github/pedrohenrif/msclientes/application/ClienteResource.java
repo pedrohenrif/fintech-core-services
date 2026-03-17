a7e88c6eedb0886d2abae5518569475581eb452c
Pedro <pedrofurtado@ghr.com>
2026-03-17 10:29:07 -0300

    feat(msclientes): adiciona endpoints para criar e buscar clientes via cpf

--- msclientes/src/main/java/io/github/pedrohenrif/msclientes/application/ClienteResource.java
+++ msclientes/src/main/java/io/github/pedrohenrif/msclientes/application/ClienteResource.java
@@ -1,15 +1,48 @@
 package io.github.pedrohenrif.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.github.pedrohenrif.msclientes.application.representation.ClienteSaveRequest;
+import io.github.pedrohenrif.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClienteResource {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+        Cliente cliente = request.toModel();
+        service.save(cliente);
+        
+        URI headerLocation = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .query("cpf={cpf}")
+            .buildAndExpand(cliente.getCpf())
+            .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity <Cliente> dadosCliente(@RequestParam String cpf) {
+        var cliente = service.getByCPF(cpf);
+
+        if(cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente.get());
     }
 }
