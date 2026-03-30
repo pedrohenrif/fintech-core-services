0b4954c3ae2c42496bd392c7f074b9f9fbfba1bd
Pedro <pedrofurtado@ghr.com>
2026-03-30 10:47:18 -0300

    feat(avaliador): expoem rota de situacao do cliente com tratamento de excecoes

--- msavaliadorcredito/src/main/java/io/github/pedrohenrif/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/pedrohenrif/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -2,10 +2,15 @@ package io.github.pedrohenrif.msavaliadorcredito.application;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import feign.Feign;
+import feign.FeignException;
 import feign.Response;
+import io.github.pedrohenrif.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
+import io.github.pedrohenrif.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
 import io.github.pedrohenrif.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.pedrohenrif.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.pedrohenrif.msavaliadorcredito.domain.model.SituacaoCliente;
@@ -20,14 +25,25 @@ public class AvaliadorCreditoService {
     private final ClienteResourceClient clientesClient;
     private final CartoesResourceClient cartoesClient;
     
-    public SituacaoCliente obterSituacaoCliente(String cpf) {
+    public SituacaoCliente obterSituacaoCliente(String cpf) 
+        throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
         
-        ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf); 
-        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
+        try{
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf); 
+            ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
-        return SituacaoCliente.builder()
-            .cliente(dadosClienteResponse.getBody())
-            .cartoes(cartoesResponse.getBody())
-            .build();
+            return SituacaoCliente.builder()
+                .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
+                .build();
+
+        }catch (FeignException.FeignClientException e){
+            int status = e.status();
+            if(HttpStatus.NOT_FOUND.value() == status){
+                throw new DadosClienteNotFoundException();
+            }
+
+            throw new  ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
     }
 }   
---
+++ msavaliadorcredito/src/main/java/io/github/pedrohenrif/msavaliadorcredito/application/ex/ErroComunicacaoMicroservicesException.java
@@ -0,0 +1,16 @@
+package io.github.pedrohenrif.msavaliadorcredito.application.ex;
+
+import lombok.Getter;
+
+
+public class ErroComunicacaoMicroservicesException extends Exception {
+
+    @Getter
+    private Integer status;
+
+    public ErroComunicacaoMicroservicesException(String msg, Integer status) {
+        super(msg);
+        this.status = status;
+    }
+
+}
